aba2d2011b3140b2f8bb5912c5bbbdaaddfefda6
JSecher <Joachim@Secher-fam.dk>
2021-10-12 11:32:59 +0200

    Initial read from input works

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-
-def read_input():
-    raise NotImplementedError
+import re
 
 
 def decoder():
-    raise NotImplementedError
+    n = int(input())
+    s = str(input())
+    t = []  # list for all t strings
+    r = dict()  # dict for all R sets
+    for _ in range(n):
+        t.append(str(input()))
+    while True:
+        try:
+            ri = str(input())
+            gamma = ri[0]
+            expansions = ri[2:].split(",")
+            r[gamma] = expansions
+        except EOFError:
+            break
+
+    return s, t, r
 
 
 def superstringwithexpansion():
-    raise NotImplementedError
-
+    inputs = decoder()
+    if inputs:
+        s, t, r = inputs
+    else:
+        return "NO"
+    
 
 if __name__ == '__main__':
 
     result = superstringwithexpansion()
 
-    if result == "No":
-        print("No")
+    print(result)
+    """
+    if result == "NO":
+        print(result)
     else:
         for res in result:
             print(f"{res[0]}:{res[1]}")
+    """
