1034e474d5494e3ef6f0c2942fe1ac0611d43847
JSecher <Joachim@Secher-fam.dk>
2021-10-12 10:47:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+def read_input():
+    
+
+
+def decoder():
+    return NotImplementedError
+
+
+def superstringwithexpansion():
+    return NotImplementedError
+
+
+if __name__ == '__main__':
+
+    result = superstringwithexpansion()
+
+    if result == "No":
+        print("No")
+    else:
+        for res in result:
+            print(f"{res[0]}:{res[1]}")
